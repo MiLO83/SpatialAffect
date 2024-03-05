a2f41e5832bcf75dcb846dd64a04def541ebd68a
MiLO83 <simdiff@gmail.com>
2024-03-05 02:22:00 -0500

    CameraControls

--- Assets/CameraControls.cs
+++ Assets/CameraControls.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class CameraControls : MonoBehaviour
 {
     public int frameCount = 0;
-    //public float speed = 0.00001f; // Controls camera movement speed
-    //public float sensitivity = 0.00001f; // Controls mouse sensitivity for camera rotation
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +14,6 @@ public class CameraControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (frameCount++ < 2048)
-        {
-            // Move the camera forward, backward, left, and right
-            //transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
-            //transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-            //float xRot = speed * Input.GetAxis("Mouse X") * sensitivity;
-            //float yRot = speed * Input.GetAxis("Mouse Y") * sensitivity;
-            //transform.Rotate(360f / (60f * 30f), 360f / (60f * 30f), 360f / (60f * 30f));
-            transform.Rotate(Random.Range(-180, 180), Random.Range(-180, 180), Random.Range(-180, 180));
-        }
+        transform.Rotate(Random.Range(-180, 180), Random.Range(-180, 180), Random.Range(-180, 180));
     }
 }
